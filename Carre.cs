4b94dba670cf9858c9842186b66adb8bb2637959
Ismael Gansonre <gansonre@MATINF.uqtr.ca>
2021-10-28 06:44:11 -0400

    Ajoutez des fichiers projet.

---
+++ Carre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Sudoku
+{
+	public class Square
+	{
+		public TextBox TextBox { get; set; }
+		public string Notes { get; set; }
+		public bool Locked { get; set; }
+
+		
+
+        public Square(System.Collections.Generic.Dictionary<string, Square> textBox)
+        {
+			
+		}
+    }
+}
